c3008e36da91313b289036091032f6507b908a93
Dian Hanifudin Subhi <dhanifudin@gmail.com>
2022-11-14 19:39:18 +0900

    ignore floating window

--- xmonad.hs
+++ xmonad.hs
@@ -1,42 +1,32 @@
-import System.Exit
-import System.IO
-import XMonad
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.EwmhDesktops
-import XMonad.Hooks.ManageDocks
-import XMonad.Hooks.ManageHelpers
-import XMonad.Hooks.SetWMName
-import XMonad.Layout.Grid
-import XMonad.Layout.IM
-import XMonad.Layout.NoBorders
-import XMonad.Layout.PerWorkspace
-import XMonad.Layout.Reflect
-import XMonad.Layout.Tabbed
-import XMonad.Layout.TwoPane
-import XMonad.Prompt
-import XMonad.Util.Cursor
-import XMonad.Util.EZConfig(additionalKeys)
-import XMonad.Util.Replace
-import XMonad.Util.Run(spawnPipe)
-import qualified Data.Map as M
-import qualified XMonad.StackSet as W
+import qualified Data.Map                     as M
+import           Data.Maybe                   (fromJust, isNothing)
+import           System.Exit
+import           System.IO
+import           XMonad
+import           XMonad.Actions.CopyWindow
+import           XMonad.Actions.GridSelect
+import           XMonad.Actions.MouseResize
+import           XMonad.Hooks.DynamicLog
+import           XMonad.Hooks.EwmhDesktops
+import           XMonad.Hooks.ManageDocks
+import           XMonad.Hooks.ManageHelpers
+import           XMonad.Hooks.SetWMName
+import           XMonad.Layout.Grid
+import           XMonad.Layout.IM
+import           XMonad.Layout.NoBorders
+import           XMonad.Layout.PerWorkspace
+import           XMonad.Layout.Reflect
+import           XMonad.Layout.Tabbed
+import           XMonad.Layout.TwoPane
+import           XMonad.Layout.WindowArranger
+import           XMonad.Prompt
+import qualified XMonad.StackSet              as W
+import           XMonad.Util.Cursor
+import           XMonad.Util.EZConfig         (additionalKeys)
+import           XMonad.Util.Replace
+import           XMonad.Util.Run              (spawnPipe)
 
 myFont                      = "xft:Fira Code-10:bold"
-ppTitleFgColor              = "#FFFFFF"
-ppTitleBgColor              = "#333333"
-ppCurrentFgColor            = "#1793D1"
-ppCurrentBgColor            = "#333333"
-ppUrgentFgColor             = "#FF0000"
-ppUrgentBgColor             = "#333333"
-ppHiddenFgColor             = "#999999"
-ppHiddenBgColor             = "#333333"
-ppSepFgColor                = ""
-ppSepBgColor                = ""
-ppWsSepFgColor              = "#1793D1"
-ppWsSepBgColor              = ""
-
-ppSeparator                 = ""
-ppWsSeparator               = ""
 
 myNormalBorderColor         = "#333333"
 myFocusedBorderColor        = "#0088CC"
@@ -44,49 +34,58 @@ myFocusedBorderColor        = "#0088CC"
 myBorderWidth               = 4
 myModMask                   = mod4Mask
 myTerminal                  = "kitty"
-myWorkspaces                = ["Q","W","E","R","T","Y","U","I","O", "P"]
+myWorkspaces                = ["Q","W","E","R","T","Y","U","I","O","P"]
 
 myManageHook                = composeAll
-    [ className             =? "TelegramDesktop"       --> doShift "P"
-    , className             =? "Slack"          --> doShift "P"
-    , className             =? "firefox"        --> doShift "W"
-    , className             =? "Toolkit"        --> doFloat
-    ]
+  [ className             =? "TelegramDesktop" --> doShift "P"
+  , className             =? "Slack" --> doShift "P"
+  , className             =? "firefox" <&&> resource =? "Toolkit" --> doFloat
+  , className             =? "mpv"  --> doRectFloat (W.RationalRect 0.25 0.25 0.5 0.5)
+  ]
 
-myLayoutHook                = onWorkspace "Q" termLayout $ onWorkspace "W" webLayout $  onWorkspace "P" messageLayout $ defaultLayout
+myLayoutHook                = mouseResize $ windowArrange $ onWorkspace "Q" termLayout $ onWorkspace "P" messageLayout $ defaultLayout
     where
-        defaultLayout       = avoidStruts ( TwoPane (3/100) (1/2) ||| tall ||| Full )
-            where
-                tall        = Tall nmaster delta ratio
-                nmaster     = 1
-                ratio       = 2/3
-                delta       = 2/100
+      defaultLayout       = avoidStruts ( TwoPane (3/100) (1/2) ||| tall ||| Full)
+          where
+              tall        = Tall nmaster delta ratio
+              nmaster     = 1
+              ratio       = 2/3
+              delta       = 1/40
 
-        termLayout          = avoidStruts ( Grid ||| Full )
+      termLayout          = avoidStruts ( TwoPane (3/100) (1/2) ||| Grid ||| Full )
 
-        webLayout           = avoidStruts ( Mirror tall ||| tall ||| Full)
-            where
-                tall        = Tall nmaster delta ratio
-                nmaster     = 1
-                ratio       = 3/4
-                delta       = 2/100
+      messageLayout       = avoidStruts ( TwoPane (3/100) (1/2) ||| Full )
 
-        messageLayout       = avoidStruts ( TwoPane (3/100) (1/2) ||| Full )
+skipFloating :: (Eq a, Ord a) => W.StackSet i l a s sd -> (W.StackSet i l a s sd -> W.StackSet i l a s sd) -> W.StackSet i l a s sd
+skipFloating stacks f
+    | isNothing curr = stacks -- short circuit if there is no currently focused window
+    | otherwise = skipFloatingR stacks curr f
+  where curr = W.peek stacks
+
+skipFloatingR :: (Eq a, Ord a) => W.StackSet i l a s sd -> (Maybe a) -> (W.StackSet i l a s sd -> W.StackSet i l a s sd) -> W.StackSet i l a s sd
+skipFloatingR stacks startWindow f
+    | isNothing nextWindow = stacks -- next window is nothing return current stack set
+    | nextWindow == startWindow = newStacks -- if next window is the starting window then return the new stack set
+    | M.notMember (fromJust nextWindow) (W.floating stacks) = newStacks -- if next window is not a floating window return the new stack set
+    | otherwise = skipFloatingR newStacks startWindow f -- the next window is a floating window so keep recursing (looking)
+  where newStacks = f stacks
+        nextWindow = W.peek newStacks
 
 myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
     [ ((0, 0x1008ff12), spawn "amixer set Master toggle")
     , ((0, 0x1008ff11), spawn "amixer set Master 5%-")
     , ((0, 0x1008ff13), spawn "amixer set Master 5%+")
     , ((modMask, xK_space), spawn "rofi -show run")
+    , ((modMask, xK_g), goToSelected def)
     , ((modMask, xK_c), spawn "clipmenu")
     , ((modMask, xK_s), spawn "scrot --delay 1 ~/Pictures/screenshots/screen_%Y-%m-%d %H-%M-%S.png")
     , ((modMask .|. shiftMask, xK_s), spawn "scrot --select ~/Pictures/screenshots/window_%Y-%m-%d %H-%M-%S.png")
     , ((modMask, xK_Return), spawn $ XMonad.terminal conf)
-    , ((modMask, xK_x), kill)
+    , ((modMask, xK_x), kill1)
     , ((modMask, xK_Tab), sendMessage NextLayout)
     , ((modMask .|. shiftMask, xK_Tab), setLayout $XMonad.layoutHook conf)
-    , ((modMask, xK_j), windows W.focusDown)
-    , ((modMask, xK_k), windows W.focusUp)
+    , ((modMask, xK_j), windows (\s -> skipFloating s W.focusDown))
+    , ((modMask, xK_k), windows (\s -> skipFloating s W.focusUp))
     , ((modMask, xK_m), windows W.focusMaster)
     , ((modMask .|. shiftMask, xK_m), windows W.swapMaster)
     , ((modMask .|. shiftMask, xK_j), windows W.swapDown)
@@ -103,7 +102,7 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
 
     [((m .|. modMask, k), windows $ f i)
         | (i, k) <- zip (XMonad.workspaces conf) [xK_q, xK_w, xK_e, xK_r, xK_t, xK_y, xK_u, xK_i, xK_o, xK_p]
-        , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+        , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask), (copy, controlMask)]]
 
 myFocusFollowsMouse :: Bool
 myFocusFollowsMouse = False
@@ -119,18 +118,17 @@ myStartupHook = do
     setDefaultCursor xC_left_ptr >> setWMName "LG3D"
     spawn "~/.xmonad/autostart"
 
-main = do
-    xmonad $ docks $ ewmh def
-        { terminal              = myTerminal
-        , borderWidth           = myBorderWidth
-        , focusFollowsMouse     = myFocusFollowsMouse
-        , modMask               = myModMask
-        , workspaces            = myWorkspaces
-        , normalBorderColor     = myNormalBorderColor
-        , focusedBorderColor    = myFocusedBorderColor
-        , keys                  = myKeys
-        , mouseBindings         = myMouseBindings
-        , layoutHook            = myLayoutHook
-        , manageHook            = manageDocks <+> myManageHook
-        , startupHook           = myStartupHook
-        }
+main = xmonad $ docks $ ewmhFullscreen . ewmh $ def
+  { terminal              = myTerminal
+  , borderWidth           = myBorderWidth
+  , focusFollowsMouse     = myFocusFollowsMouse
+  , modMask               = myModMask
+  , workspaces            = myWorkspaces
+  , normalBorderColor     = myNormalBorderColor
+  , focusedBorderColor    = myFocusedBorderColor
+  , keys                  = myKeys
+  , mouseBindings         = myMouseBindings
+  , layoutHook            = myLayoutHook
+  , manageHook            = manageDocks <+> myManageHook
+  , startupHook           = myStartupHook
+  }
